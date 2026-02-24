d5ba99367c9c2f056765107613ed871936b011e8
wasp32679 <ryan.kenmoe@outlook.fr>
2026-02-24 10:31:07 +0100

    feat: finished getQueryParameters

--- public/queryParameterDisplay.php
+++ public/queryParameterDisplay.php
@@ -1,24 +1,26 @@
 <?php
-/**
- * Get the values from the GET parameters with filter_input function
- */
+$name = filter_input(INPUT_GET, "name");
+$age = filter_input(INPUT_GET, "age", FILTER_VALIDATE_INT);
 
 ?>
 
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>URL query parameters</title>
 </head>
+
 <body>
-
-<!-- Display parameters here in a h1 tag -->
-
-<!-- Display message in list element in case of missing parameters -->
-
+    <?php if ($name && $age): ?>
+        <h1><?= $name ?> is <?= $age ?> years old</h1>
+    <?php else: ?>
+        <li>missing parameter</li>
+    <?php endif; ?>
 </body>
+
 </html>
